6ace149238fe725123adb2294b1573a5f654c2d8
fayentss <117296851+fayentss@users.noreply.github.com>
2023-11-23 09:05:34 +0100

    blabla

--- TowerDefense UML/Ennemies.h
+++ TowerDefense UML/Ennemies.h
@@ -9,12 +9,7 @@ private :
 		minDamage, maxDamage, minSpeed, 
 		maxSpeed, minGold, maxGold;
 public:
-	Ennemies(Vect2 pos, Vect2 size, float color, int speed, int life, int damage, int ID, int minLife, int maxLife, int minDamage, int maxDamage, int minSpeed, int maxSpeed, int minGold, int maxGold);
-
-	int randSpeed(int minSpeed, int maxSpeed);
-	int randLife(int minLife, int maxLife);
-	int randGold(int minGold, int maxGold);
-	int randDamage(int minDamage, int maxDamage);
+	Ennemies(Vect2 pos, Vect2 size, float color, int speed, int life, int damage, int ID, int iLife, int iDamage, int iSpeed, int iGold);
 
 };
 
