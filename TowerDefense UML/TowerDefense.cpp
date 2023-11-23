3391f346d905d556180386bde27e30189ea53ddf
ebeaufils <ebeaufils@GCFRLY257>
2023-11-23 09:02:58 +0100

    update : main

--- TowerDefense UML/TowerDefense.cpp
+++ TowerDefense UML/TowerDefense.cpp
@@ -3,16 +3,18 @@
 
 #include "FileReader.h"
 #include "Window.h"
+#include "GameManager.h"
+#include "EventManager.h"
+
+void Game()
+{
+	GameManager::Get()->game();
+}
 
 int main()
 {
-    FileReader* o_file = new FileReader();
-
-    o_file->readFileTxt("Files/test.txt");
-
-    MWindow* o_window = new MWindow(Vect2(1000, 800), "Tower Defence");
-
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-
+	EventManager::Create();
+	GameManager::Create();
+	Game();
 }
 
